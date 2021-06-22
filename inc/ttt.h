3e7979714f30be01ce09223be863a7e1cf940dbe
Patrick Mims <patrickkevinmims@gmail.com>
2021-06-22 07:58:01 -0700

    Fixed tth

--- inc/ttt.h
+++ inc/ttt.h
@@ -1,34 +1,92 @@
 #ifndef _TTT_H
 #define _TTT_H
-#define WIDTH 5
+#define WIDTH 1
 #define LENGTH 3
 
 using namespace std;
 
+int checkBoundary(int a)
+{
+    if ((a < 3))
+        return 1;
+
+    return 0;
+}
+
 void displayBoard(char board[][3])
 {
     int cnt = 0;
-    do
+    int i;
+    int j = 0;
+    int a;
+    int b;
+    //do
+    // {
+    for (i = 0; i < LENGTH; i++)
     {
-        for (int i = 0; i <= LENGTH; i++)
+        cout << "-";
+        for (j = 0; j < LENGTH; j++)
         {
-            for (int j = 0; j <= LENGTH; j++)
+            cout << "-";
+        }
+        cout << "-";
+    }
+    cout << endl;
+    /*
+        if (cnt < LENGTH)
+            cout << "| "
+                 << board[i][j]
+                 << " | "
+                 << endl;
+                 */
+    for (a = 0; a < LENGTH; a++)
+    {
+        cout << "| ";
+        for (b = 0; b < LENGTH; b++)
+        {
+            //cout << board[a][b];
+            cout << board[a][b] << " | ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    /*
+                 << "B"
+                 << " | "
+                 << "C"
+                 << " |" << endl;
+                 */
+    cnt += 1;
+    //} while (cnt <= LENGTH);
+
+    /*
+        for (int a = 0; a <= LENGTH; a++)
+        {
+            for (int b = 0; b <= LENGTH; b++)
             {
-                cout << "-";
+                cout << board[a][b];
             }
         }
         if (cnt < LENGTH)
             cout << endl
                  << "|" << setw(WIDTH) << "|" << setw(WIDTH) << "|" << setw(WIDTH) << "|" << endl;
+                 */
 
-        cnt += 1;
-    } while (cnt <= LENGTH);
     cout << endl;
 }
 
-void makeAMove(char[][3], char)
+void makeAMove(char move[][3], char mark)
 {
+    int c = 0, r = 0;
     cout << "Enter a row (0, 1, 2) for player X: " << endl;
+    cin >> r;
+
+    if (checkBoundary(r))
+    {
+        cout << "Enter a column (0, 1, 2) for player X: " << endl;
+        cin >> c;
+    }
+    move[c][r] = mark;
 }
 
 /*
